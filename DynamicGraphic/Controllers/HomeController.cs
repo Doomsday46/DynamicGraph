44de828258588adf5e08cc621386a3c2bd22937b
Vitaliy <Gallifrey46@gmail.com>
2018-10-20 16:12:18 +0300

    Алгоритмы генерация записей вынесены из контроллера в стратегию. Добавлена случайная генерация цветов для графика. Рефакторинг.

--- DynamicGraphic/Controllers/HomeController.cs
+++ DynamicGraphic/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using DynamicGraphic.Mappers;
 using DynamicGraphic.Models;
 using DynamicGraphic.Models.Generators;
 using DynamicGraphic.Models.Generators.Implements;
+using DynamicGraphic.Strategies;
+using DynamicGraphic.Strategies.Enum;
 using Ninject;
 using System;
 using System.Collections.Generic;
@@ -15,55 +17,34 @@ namespace DynamicGraphic.Controllers
 {
     public class HomeController : Controller
     {
-        [Inject]
-        public IRepository Repository { get; set; }
 
-        public IAddMeasurementRecords addMeasurementRecords;
+        public IRepository Repository { get; set; }
 
         [Inject]
         public IMapper ModelMapper { get; set; }
+
+        private ExecuterStrategies executerStratigies;
+        [Inject]
+        public HomeController(IRepository Repository) {
+            this.Repository = Repository;
+            executerStratigies = new ExecuterStrategies(new List<IStrategy>() { new GenerateRecords(Repository), new RandomGenerateRecords(Repository) });
+        }
+
         public ActionResult Index()
         {
-
+            
             return View();
         }
 
-        private void RandomGenerateRecords()
-        {
-            Random random = new Random();
-            int size_measurements = random.Next(1, 15),
-                size_string = random.Next(4, 6);
-            ICollection<Measurement> measurements = GetMeasurements(size_measurements);
-            addMeasurementRecords = new RandomAddMeasurementRecords(measurements, new RandomString(random, size_string));
-            Repository.addAllMeasurements(addMeasurementRecords.getRecords());
-        }
-        private void GenerateRecords()
-        {
-            Random random = new Random();
-            List<string> list_names_parameters = new List<string>();
-            for (var i = 1; i < 12; i++)
-                list_names_parameters.Add("Param" + i);
-            ICollection<Measurement> measurements = GetMeasurements(random.Next(1, 15));
-            addMeasurementRecords = new RandomAddMeasurementRecords(measurements, new RandomByTheList(random, list_names_parameters));
-            Repository.addAllMeasurements(addMeasurementRecords.getRecords());
-        }
         public  ActionResult GenerateRecordsAsync() {
             int startTimeSpan = 5000,
                 periodTimeSpan = 20000;
             var timer = new Timer(async (e) =>
             {
-                await Task.Run(() => GenerateRecords());
+                await Task.Run(() => executerStratigies.ExecuteStrategy(AlgorithmEnum.GenerateRecords));
             }, null, startTimeSpan, periodTimeSpan);
 
             return View("Index");
-        }
-
-        private ICollection<Measurement> GetMeasurements(int size) {
-            ICollection<Measurement> measurements = new List<Measurement>();
-            for (int i = 0; i < size; i++) {
-                measurements.Add(new MeasurementFactory().GetMeasurement());
-            }
-            return measurements;
         }
 
         public ActionResult GetData()
---
+++ DynamicGraphic/Strategies/RandomGenerateRecords.cs
@@ -0,0 +1,34 @@
+﻿using DynamicGraphic.App_Data;
+using DynamicGraphic.Models;
+using DynamicGraphic.Models.Generators.Implements;
+using DynamicGraphic.Strategies.Enum;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicGraphic.Strategies
+{
+    public class RandomGenerateRecords : IStrategy
+    {
+        public RandomGenerateRecords(IRepository repository) : base(repository)
+        {
+        }
+
+        public override void ExecuteAlgorithm()
+        {
+            Random random = new Random();
+            int size_measurements = random.Next(1, 15),
+                size_string = random.Next(4, 6);
+            ICollection<Measurement> measurements = GetMeasurements(size_measurements);
+            addMeasurementRecords = new RandomAddMeasurementRecords(measurements, new RandomString(random, size_string));
+            Repository.addAllMeasurements(addMeasurementRecords.getRecords());
+        }
+
+        public override AlgorithmEnum GetTyperAlgoritm()
+        {
+            return AlgorithmEnum.RandomGenerateRecords;
+        }
+    }
+}
